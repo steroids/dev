4b7fada89130083d75a79e17be6676edfeb6f8b8
SergeiKaluckij <sergkaluckij@gmail.com>
2020-09-01 17:47:23 +0700

    Notification

--- mobile/src/routes/CheckBoxScreen/CheckBoxScreen.tsx
+++ mobile/src/routes/CheckBoxScreen/CheckBoxScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Text, View} from 'react-native'
+import CheckboxField from "@steroidsjs/core/ui/form/CheckboxField";
 
 export default class CheckBoxScreen extends React.Component<any, any> {
     render() {
         return (
             <View>
-                <Text>CheckBox</Text>
+
             </View>
         )
     }

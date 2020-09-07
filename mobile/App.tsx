dfe9e762f983238d3dd4789b1fe94b20ea1797af
SergeiKaluckij <sergkaluckij@gmail.com>
2020-09-07 11:33:26 +0700

    Test 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -22,6 +22,9 @@ import style from './style';
             'form.DateFieldView': require('@steroidsjs/native/ui/form/DateField').default,
             'form.TimeFieldView': require('@steroidsjs/native/ui/form/TimeField').default,
             'form.DateTimeFieldView': require('@steroidsjs/native/ui/form/DateTimeField').default,
+            'form.CheckboxFieldView': require('@steroidsjs/native/ui/form/CheckboxField').default,
+            'form.CheckboxListFieldView': require('@steroidsjs/native/ui/form/CheckboxListField').default,
+            'form.RadioListFieldView': require('@steroidsjs/native/ui/form/RadioListField').default,
             'layout.NotificationsView': require('@steroidsjs/native/ui/layout/Notification').default,
             'layout.NotificationsItemView': require('@steroidsjs/native/ui/layout/Notification/NotificationItemView').default,
         });
--- mobile/src/routes/CheckBoxScreen/CheckBoxScreen.tsx
+++ mobile/src/routes/CheckBoxScreen/CheckBoxScreen.tsx
@@ -1,12 +1,50 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {View} from 'react-native'
 import CheckboxField from "@steroidsjs/core/ui/form/CheckboxField";
+import CheckboxListField from "@steroidsjs/core/ui/form/CheckboxListField";
+import RadioListField from "@steroidsjs/core/ui/form/RadioListField";
 
 export default class CheckBoxScreen extends React.Component<any, any> {
     render() {
         return (
-            <View>
-
+            <View style={{
+                height: '100%',
+                width: '100%',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                backgroundColor: '#242125'
+            }}>
+                <View>
+                    <CheckboxField label='Учитывать параметры моей анкеты'/>
+                </View>
+                <View>
+                    <CheckboxListField
+                        items={[
+                            {label: 'Америка'},
+                            {label: 'Африка'},
+                            {label: 'Европа'},
+                            {label: 'Карибское море'},
+                        ]}
+                        multiple={true}
+                    />
+                </View>
+                <View>
+                    <RadioListField
+                        items={[
+                                {label: 'Мужской'},
+                                {label: 'Женский'},
+                                {label: 'Не важно'}
+                            ]}
+                    />
+                </View>
+                <View>
+                    <RadioListField
+                        items={[
+                            {label: 'Страна или Город'},
+                            {label: 'Круизы'}
+                        ]}
+                    />
+                </View>
             </View>
         )
     }

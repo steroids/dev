e68b05c2353db50a6e27cfff199bd4568195c182
Vladimir Kozhin <hello@kozhindev.com>
2021-03-24 17:03:21 +0700

    frontend upds

--- frontend/Application.tsx
+++ frontend/Application.tsx
@@ -3,12 +3,18 @@ import * as React from 'react';
 import useApplication from '@steroidsjs/core/hooks/useApplication';
 
 import 'style/index.scss';
+import LocaleComponent from '@steroidsjs/core/components/LocaleComponent';
 
 export default function Application() {
     const {renderApplication} = useApplication({
         reducers: require('@steroidsjs/core/reducers').default,
         routes: require('routes').default,
         layoutView: require('shared/Layout').default,
+        components: {
+            locale: {
+               className: LocaleComponent,
+            },
+        },
         onInit: ({ui}) => {
             ui.addViews(require('@steroidsjs/bootstrap').default);
             //ui.addFields(require('@steroidsjs/core/ui/form').default);

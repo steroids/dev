3f01609b9e460f2d4ff65dc6068db93100151271
Stiivenson <nicklihorus4@gmail.com>
2021-04-01 15:17:02 +0700

    upd Tooltip

--- frontend/shared/Layout/Layout.tsx
+++ frontend/shared/Layout/Layout.tsx
@@ -42,7 +42,7 @@ export default function Layout(props) {
             <div className={bem.element('content')}>
                 {renderContent()}
                 <Notifications/>
-                {/*<Portal/>*/}
+                <Portal/>
             </div>
         </div>
     );

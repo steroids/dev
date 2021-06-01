b7671152b4dec520efbef870eb64e7eeb669f88f
antonovaekaterina <antonova.ekaterina19@yandex.ru>
2021-06-01 19:38:54 +0700

    add ssr webpack config

--- webpack.js
+++ webpack.js
@@ -1,5 +1,16 @@
 process.env.APP_BACKEND_URL = process.env.APP_BACKEND_URL || 'https://steroids.kozhindedev.com';
 
+const webpackConfig = {
+    resolve: {
+        alias: {
+            '@steroidsjs/core/build': __dirname + '/steroids/react/build',
+            '@steroidsjs/core/docs': __dirname + '/steroids/react/docs',
+            '@steroidsjs/core': __dirname + '/steroids/react/src',
+            '@steroidsjs/bootstrap': __dirname + '/steroids/react-bootstrap/src',
+        },
+    },
+};
+
 require('./steroids/react-webpack')
     .config({
         port: 9991,
@@ -8,15 +19,7 @@ require('./steroids/react-webpack')
         baseUrl: 'frontend/',
         inlineSvg: true,
         serverPath: './steroids/ssr',
-        webpack: {
-            resolve: {
-                alias: {
-                    '@steroidsjs/core/build': __dirname + '/steroids/react/build',
-                    '@steroidsjs/core/docs': __dirname + '/steroids/react/docs',
-                    '@steroidsjs/core': __dirname + '/steroids/react/src',
-                    '@steroidsjs/bootstrap': __dirname + '/steroids/react-bootstrap/src',
-                },
-            },
-        },
+        webpack: webpackConfig,
+        ssr: webpackConfig
     })
     .base(__dirname + '/frontend/index.tsx');
